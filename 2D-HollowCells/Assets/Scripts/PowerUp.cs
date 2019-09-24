9a3f7980e3c5f89ae668da6bb608aa0c14b96981
Alek2750 <alek2750@gmail.com>
2019-09-24 11:51:53 +0200

    PowerUpSpeed works

--- 2D-HollowCells/Assets/Scripts/PowerUp.cs
+++ 2D-HollowCells/Assets/Scripts/PowerUp.cs
@@ -1,24 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets._2D;
 
 public class PowerUp : MonoBehaviour
 {
-
     public GameObject pickupEffect;
+
+    public float speedBoost = 1.5f;
+    //public float powerupTime = 5f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.CompareTag("Player"))
         {
-            Pickup();
+            StartCoroutine(Pickup(collision));
         }
     }
 
-    void Pickup()
+    IEnumerator Pickup(Collider2D player)
     {
-
+        // spawn effect
         Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        
+        PlatformerCharacter2D speed = player.GetComponent<PlatformerCharacter2D>();
+        speed.m_MaxSpeed *= speedBoost;
+
+        GetComponent<SpriteRenderer>().enabled = false; // disable Sprite
+        GetComponent<Collider2D>().enabled = false; // disable collider
+
+        yield return new WaitForSeconds(10f); // wait for 5s
+
+        speed.m_MaxSpeed /= speedBoost;
+
         Destroy(gameObject);
     }
+
+    
 }
