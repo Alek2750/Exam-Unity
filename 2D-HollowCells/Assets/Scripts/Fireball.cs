0c2c5caf88407089a23dab11f27f5dedf49e3041
Alek2750 <alek2750@gmail.com>
2019-09-25 14:54:41 +0200

    Kirby can shoot 1 way

---
+++ 2D-HollowCells/Assets/Scripts/Fireball.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class Fireball : MonoBehaviour
+{
+    [SerializeField] private int damageAmount;
+
+    public int health = 100;
+    private PlatformerCharacter2D player;
+    public GameObject deathEffect;
+    public float speed;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlatformerCharacter2D>();
+
+        rb = GetComponent<Rigidbody2D>();
+
+        if (player.transform.position.x < transform.position.x)
+        {
+            speed = -speed;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+    }
+
+    
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        Player player = collider.GetComponent<Player>();
+        if (collider.CompareTag("Player"))
+        {
+
+            player.DamageTake(damageAmount);
+        }
+        Instantiate(deathEffect, transform.position, transform.rotation);
+        
+        Destroy(gameObject);
+    }
+
+}
---
+++ 2D-HollowCells/Assets/Scripts/PlayerInRange.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class PlayerInRange : MonoBehaviour
+{
+
+    public float playerRange;
+
+    public GameObject fireBall;
+
+    public PlatformerCharacter2D player;
+
+    public Transform shotPoint;
+
+    public float waitBewteenShots;
+    private float shotCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlatformerCharacter2D>();
+        shotCounter = waitBewteenShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(new Vector3(transform.position.x - playerRange, transform.position.y, transform.position.z), new Vector3(transform.position.x + playerRange, transform.position.y, transform.position.z));
+        shotCounter -= Time.deltaTime;
+
+        if (transform.rotation.y > 0 && player.transform.position.x < transform.position.x && player.transform.position.x > transform.position.x - playerRange && shotCounter < 0)
+        {
+            Instantiate(fireBall, shotPoint.position, shotPoint.rotation);
+            shotCounter = waitBewteenShots;
+        }
+
+        if (transform.rotation.y < 0 && player.transform.position.x > transform.position.x && player.transform.position.x < transform.position.x + playerRange && shotCounter < 0)
+        {
+            Instantiate(fireBall, shotPoint.position, shotPoint.rotation);
+            shotCounter = waitBewteenShots;
+        }
+    }
+}
